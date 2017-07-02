31d3c9116518ab62634a8a4717676cc948a43406
Serafín Maza Domínguez <serafin@Serafins-MacBook-Pro.local>
2017-07-02 20:39:51 +0200

    Beautified code + added \'Click on country for more information\' text on the popup.

--- scripts/getText.js
+++ scripts/getText.js
@@ -1,7 +1,11 @@
 function getText(countryCode) {
-    if (countryData[0][countryCode].text === null) {
+    if (countryData[0][countryCode].text == null) {
         return "No data available for this country";
     } else {
-        return countryData[0][countryCode].text;
+        var text = countryData[0][countryCode].text;
+        if (countryData[0][countryCode].link != null) {
+            text = text + " Click on country for more information."
+        }
+        return text;
     }
 }
